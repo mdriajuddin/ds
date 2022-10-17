6a5cbf1f1cde0e082b5003ddf2c9c164bc3cc31a
Riaj <mdriajuddin2001@gmail.com>
2022-10-17 12:26:37 +0600

    Array2D implemented

--- array/array.py
+++ array/array.py
@@ -46,4 +46,60 @@ class _ArrayIterator:
             return entry
         else:
             raise StopIteration
+
+
+
+
+# Implementation of the Array2D ADT using an array of array.
+class Array2D:
+    # Creates a 2-D array of size numRows x numCols.
+    def __init__(self, numnRows, numCols) -> None:
+        # Create a 1-D array to store an array reference for each row.
+        self._theRows = Array(numnRows)
+
+        # Create the 1-D array to store row of the t2-D arrary.
+        for i in range(numnRows)
+        self._theRows[i] = Array(numCols)
     
+    # Returns the number of rows in the 2-D array.
+    def numRows(self):
+        return len(self._theRows)
+    
+    # Returns the number of colums in the 2-D array.
+    def numCols(self):
+        return len(self._theRows[0])
+    
+    # Clears the array by setting every element to the given value.
+    def clear(self, value):
+        for row in range(self.numRows()):
+            row.clear(value)
+    
+
+    # Gets the contents of the element at position [i, j]
+    def __getitem__(self, ndxTuple):
+        assert len(ndxTuple) == 2, "Invalid number of array subscripts."
+        row = ndxTuple[0]
+        col = ndxTuple[1]
+        assert row >= 0 and row < self.numRows() \
+            and clo >= 0 and clo < self.numCols(), \
+                "Array subscript out of range."
+        theIdAray = self._theRows[row]
+        return theIdAray[col]
+
+    
+
+    # Sets the contents of the element at position [i, j] to value.
+    def __setitem__(self, ndxTuple, value):
+        assert len(ndxTuple) == 2, "Invalid number of array subsctipts."
+        row = ndxTuple[0]
+        col = ndxTuple[1]
+        assert row >= 0 and row < self.numRows() \
+            and col >= 0 and col < self.numCols(), \
+                "Array subscript out of range."
+        theIdArray = self._theRows[row]
+        theIdArray[col] = value
+
+
+
+
+
--- array/main.py
+++ array/main.py
@@ -1,7 +1,7 @@
 # Count the number of occurences of each letter in a text file.
 
-from array import array
-from multiprocessing.dummy import Array
+from array import Array, Array2D
+
 
 # Create an array for the counters and initialize each element to 0.
 theCounters = Array(27)
@@ -23,3 +23,50 @@ for line in theFile:
     # range 65.. 90 and the lowercase letters are in the range 97..122.
     for i in range(26):
         print("%c - %4d   %c - %4d "% (chr(65+i), theCounters[65+i],chr(97+i), theCounters[97+i]))
+
+
+# 2-D array
+
+# Open the text file for reading 
+gradeFile = open("grade.txt", 'r')
+
+# Extract the fiest tow values which indicate the size of the array.
+numExams = int(gradeFile.readline())
+numStudents = int(gradeFile.readline())
+
+
+# Create the 2-D arrray to store the grades.
+examGrades = Array2D(numStudents, numExams)
+
+# Extract the frades form the remaining lines.
+i = 0
+for student in gradeFile:
+    grades = student.split()
+    for j in range(numExams):
+        examGrades[i, j] = int(grades[j])
+    i += 1
+# Close the text file.
+gradeFile.close()
+
+
+# Compute each student's acerage eam grade.
+
+for i in range(numStudents):
+    # Tally the exam grades for the ith student.
+    total = 0
+    for j in range(numExams):
+        total += examGrades[i, j]
+
+    # Compute average for the ith student.
+    examAvg = total / numExams
+    print("%2d: %6.2f"% (i+1, examAvg))
+
+
+
+
+
+
+
+
+
+
